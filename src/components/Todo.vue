646d5dd559b2856ac343babbb282d748fd17d5a4
Govern <LLLWL424@163.com>
2019-12-17 13:58:36 +0800

    fir1

---
+++ src/components/Todo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="todo-container">
+        <ol>
+            <li v-for="(item, index) in todos" :key="index">{{ item }}</li>
+        </ol>
+        <div class="input">
+            <input type="text" v-model="item">
+            <button v-on:click="submitTodo">提交</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'todo',
+        data() {
+            return {
+                todos: [
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                ],
+                item: ''
+            }
+        },
+        methods: {
+            submitTodo() {
+                // this.todos.push(this.item)
+                // [...]
+                // console.log(this.item)
+                this.todos = [...this.todos, this.item];
+                // this.todos = this.todos.concat([this.item])
+                this.item = ''
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
