1d6835b2b7ade53898b874436de98eb3a8bc91b4
Govern <LLLWL424@163.com>
2019-12-18 08:16:37 +0800

    parcel vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app">
     <todo></todo>
+    <give-like
+            v-for="(item, index) in zanData"
+            :key="index"
+            :zanData="item"
+            @state="getState"></give-like>
   </div>
 </template>
 
 <script>
   import Todo from './components/Todo'
+  import GiveLike from './components/GiveLikeComponent'
+
 
   export default {
     name: 'app',
     components: {
-      Todo
+      Todo,
+      GiveLike
     },
     data() {
-      return {}
+      return {
+        zanData: [
+          {isZan: 0, count: 10, id: 1},
+          {isZan: 1, count: 11, id: 2},
+          {isZan: 0, count: 20, id: '45678'},
+          {isZan: 1, count: 420, id: '4678'},
+
+        ]
+      }
     },
+    methods: {
+      getState(id) {
+        this.zanData.forEach(item => {
+          if(item.id === id) {
+            if (item.isZan === 1) {
+              item.isZan = 0
+              item.count = item.count - 1
+            } else {
+              item.isZan = 1
+              item.count = item.count + 1
+            }
+          }
+        })
+      }
+    }
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.component('give-like', GiveLikeComponent);
+import './assets/stylesheets/Normalize.css'
+import './assets/awesome-font/css/font-awesome.min.css'
+import GiveLikeComponent from "./components/GiveLikeComponent";
 new Vue({
   render: h => h(App),
 }).$mount('#app')
